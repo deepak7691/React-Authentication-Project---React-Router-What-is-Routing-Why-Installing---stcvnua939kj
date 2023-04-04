466f898e1067a5eb92b811b065167d4fb92f5d80
deepak7691 <119676343+deepak7691@users.noreply.github.com>
2023-04-04 15:28:38 +0530

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,1 +1,25 @@
 
+import React from "react";
+import Login from "./Login"; 
+import { useHistory } from "react-router-dom";
+
+const PrivateRoute = (props)=>{
+    let history = useHistory();
+
+    if(window.localStorage.getItem("username") && window.localStorage.getItem("password")){
+        return(
+            <>
+                {props.children}
+            </>
+        )
+    }
+    else{
+        history.push("./login");
+        return(
+            <Login />
+        )
+    }
+}
+
+export default PrivateRoute;
+
